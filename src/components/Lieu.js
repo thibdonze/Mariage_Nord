25627aff0dfc8143dacec4068329c70f15a19c5d
thibdonze <thib.donze@gmail.com>
2025-11-01 09:36:25 +0100

    v2

--- src/components/Lieu.js
+++ src/components/Lieu.js
@@ -20,7 +20,7 @@ const Lieu = () => {
             <p>Église Saint-Martin, Place de l’Église, 62129 Thérouanne, France</p>
             <p>
               <a
-                href="https://www.google.com/maps/search/?api=1&query=Église+Saint-Martin,+Thérouanne,+France"
+                href="https://goo.gl/maps/ejqk4xk3iG9rZqEJ7"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-600 hover:underline"
@@ -29,15 +29,14 @@ const Lieu = () => {
               </a>
             </p>
 
-            {/* Carte Google Maps de l'Église */}
             <div className="flex justify-center mt-4">
               <iframe
                 title="Carte de l'Église Saint-Martin de Thérouanne"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2560.869099923303!2d2.2269696769713764!3d50.73489497164071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd23963b1ef9d7%3A0xd3a6487f773c6f06!2s%C3%89glise%20Saint-Martin!5e0!3m2!1sfr!2sfr!4v1730227300000!5m2!1sfr!2sfr"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2516.652243153292!2d2.256!3d50.636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd004003eee3e9%3A0x8a06624735bbff02!2s%C3%89glise%20Saint-Martin%2C%2062129%20Th%C3%A9rouanne!5e0!3m2!1sfr!2sfr!4v1730459200000!5m2!1sfr!2sfr"
                 width="100%"
                 height="300"
                 className="rounded-lg shadow-lg"
-                allowFullScreen=""
+                allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
@@ -47,10 +46,10 @@ const Lieu = () => {
           {/* --- Salle de la Poudrerie --- */}
           <div className="space-y-3 mt-10">
             <p className="font-semibold underline text-lg">Adresse de la salle :</p>
-            <p>Salle de la Poudrerie, Rue de la Poudrerie, 62380 Esquerdes, France</p>
+            <p>Salle des fêtes de la Poudrerie, Rue Bernard Chochoy, 62380 Esquerdes, France</p>
             <p>
               <a
-                href="https://www.google.com/maps/search/?api=1&query=Salle+de+la+Poudrerie,+Rue+de+la+Poudrerie,+62380+Esquerdes,+France"
+                href="https://goo.gl/maps/ZtS64PqJScR9m9zj9"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-600 hover:underline"
@@ -59,15 +58,14 @@ const Lieu = () => {
               </a>
             </p>
 
-            {/* Carte Google Maps de la salle */}
             <div className="flex justify-center mt-4">
               <iframe
-                title="Carte de la Salle de la Poudrerie"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.295234982567!2d2.2017084!3d50.7267091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd2300fd4c82b3%3A0xe2bc97ce9877f2e2!2sSalle%20de%20la%20Poudrerie!5e0!3m2!1sfr!2sfr!4v1730227200000!5m2!1sfr!2sfr"
+                title="Carte de la Salle des fêtes de la Poudrerie - Esquerdes"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2510.170515732761!2d2.1752994151363957!3d50.70804197951407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dc55d7025ed01d%3A0x7487cdd9167b4bfb!2sSalle%20des%20f%C3%AAtes%20de%20la%20Poudrerie!5e0!3m2!1sfr!2sfr!4v1730459954321!5m2!1sfr!2sfr"
                 width="100%"
                 height="300"
                 className="rounded-lg shadow-lg"
-                allowFullScreen=""
+                allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
